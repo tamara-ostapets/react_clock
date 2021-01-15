69e11c7449bf76da04f840ae9603e31ba2e2d181
tamara ostapets <tamara.ostapec@gmail.com>
2021-01-15 12:22:16 +0200

    Updates

---
+++ precache-manifest.a32f4dc32e288037cde883efec723bb9.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b29179d62b5b3dff546ed30e968fe00",
+    "url": "./index.html"
+  },
+  {
+    "revision": "1e51852fa3ee16d6de3d",
+    "url": "./static/css/main.819e4dd6.chunk.css"
+  },
+  {
+    "revision": "9bfabb54970d8a1edebf",
+    "url": "./static/js/2.c17dec48.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.c17dec48.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1e51852fa3ee16d6de3d",
+    "url": "./static/js/main.f55fb976.chunk.js"
+  },
+  {
+    "revision": "89de80f3985b25002b5f",
+    "url": "./static/js/runtime-main.17b9eccd.js"
+  }
+]);
